645ffec892e8121f07efc1757e546cc4357b160d
abhibs1508-crypto <abhibs1508@gmail.com>
2025-11-07 12:26:42 +0530

    a0701

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="logo">MyWebsite</div>
+      <div className={`nav-links ${isOpen ? "open" : ""}`}>
+        <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+        <Link to="/about" onClick={() => setIsOpen(false)}>About</Link>
+        <Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
+      </div>
+      <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
